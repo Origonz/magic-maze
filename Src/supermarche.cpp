6bdd61cdc0d9a502f5660f6361af64f2f0157e45
Billy <vizaar@hotmail.fr>
2018-04-04 19:51:49 +0200

    Gestion nombre de Tuile

--- Src/supermarche.cpp
+++ Src/supermarche.cpp
@@ -8,13 +8,19 @@ SuperMarche::SuperMarche()
   mel = new Melangeur(sizeof(Tuile));
   map  = vector<Tuile>();
   map.push_back(Tuile(true));
-  for(int i=0;i<10;i++){
+
+  do{
       mel->inserer(new Tuile());
-  }
+      if(nb_O == 0 && nb_S == 0){
+          break;
+      }
+  }while(1);
+
 }
 
 void SuperMarche::Affiche(){
     Tuile t;
+    std::cout<<"Nombre de Tuile : "<<mel->taille()<<std::endl<<std::endl;
     for(int i=0;i<10;i++){
         mel->retirer(&t);
         std::cout<<t<<std::endl<<std::endl;
